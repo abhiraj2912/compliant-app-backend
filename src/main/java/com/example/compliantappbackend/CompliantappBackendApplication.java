eee542c8d2b5b685126e19c4f76b5069bf8ff40c
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-09 14:21:12 +0530

    first commit

---
+++ src/main/java/com/example/compliantappbackend/CompliantappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.compliantappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompliantappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompliantappBackendApplication.class, args);
+	}
+
+}
