e23daacf4e0ebdb9bba6ae1f47e64f853e8b67ca
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-09 14:34:51 +0530

    controller

--- src/main/java/com/example/compliantappbackend/controller/CompliantController.java
+++ src/main/java/com/example/compliantappbackend/controller/CompliantController.java
@@ -1,4 +1,25 @@
 package com.example.compliantappbackend.controller;
 
+import com.example.compliantappbackend.model.User;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
 public class CompliantController {
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/adduser", produces = "application/json", consumes = "application/json" )
+    public HashMap<String, String> AddUser(@RequestBody User u){
+
+        HashMap <String, String> map = new HashMap<>();
+        map.put("status","success");
+        return map;
+    }
+
+
 }
